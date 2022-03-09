4ad58ffbde5644a8483c842c4a3f920474d6d31e
Vladislav <burdukovskij.vlad@mail.ru>
2022-03-09 09:55:09 +0300

    gg

--- app/js/home.js
+++ app/js/home.js
@@ -27,3 +27,41 @@ let select = function () {
 
 
 select();
+
+
+
+
+
+//modals
+const body = document.querySelector('body');
+const modalClose = document.querySelectorAll('.modal-close');
+const openModalContact = document.querySelectorAll('.openModalContact');
+const modalBg = document.querySelector('.modal-bg');
+const modalContact = document.querySelector('.modal-contact')
+
+
+const openModal = () => {
+    modalBg.classList.add('active');
+    body.classList.add('active')
+}
+
+const closeModal = () => {
+    modalBg.classList.remove('active');
+    body.classList.remove('active')
+    modalContact.classList.remove('active')
+}
+
+openModalContact.forEach(elem => {
+    elem.onclick = () => {
+        openModal()
+        modalContact.classList.add('active');
+    }
+})
+modalBg.onclick = () => {
+    closeModal()
+}
+modalClose.forEach(elem => {
+    elem.onclick = () => {
+        closeModal()
+    }
+});
