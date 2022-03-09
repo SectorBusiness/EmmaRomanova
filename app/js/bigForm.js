e93db35979a2056d97f0261391de10f9659b0149
Vladislav <burdukovskij.vlad@mail.ru>
2022-03-09 16:34:35 +0300

    sss

---
+++ app/js/bigForm.js
@@ -0,0 +1,146 @@
+let select = function () {
+  let selectHeader = document.querySelectorAll('.select__header');
+  let selectItem = document.querySelectorAll('.select__item');
+
+  selectHeader.forEach(item => {
+    item.addEventListener('click', selectToggle)
+  });
+
+  selectItem.forEach(item => {
+    item.addEventListener('click', selectChoose)
+  });
+
+  function selectToggle() {
+    this.parentElement.classList.toggle('is-active');
+  }
+
+  function selectChoose() {
+    let text = this.innerText,
+      select = this.closest('.select'),
+      currentText = select.querySelector('.select__current');
+    currentText.innerText = text;
+    select.classList.remove('is-active');
+
+  }
+
+};
+
+
+select();
+
+
+
+const dataBigForm = [
+  {
+    mainEvent: '',
+    peoples: '',
+    longTimeEvent: '1 час',
+    dopEvent: []
+  },
+]
+const sliderTime = document.querySelector('.slider-time');
+const firstSelect = document.querySelector('.firstSelect');
+const calculateDescpItem = document.querySelectorAll('.calculate-descp p');
+const calculateServItem = document.querySelectorAll('.calculate__serv-item');
+const servInput = document.querySelectorAll('.serv-input');
+
+
+const selectEventMain = document.querySelector('.selectEvent');
+const selectEventItem = document.querySelectorAll('.selectEventItem');
+const selectCalculatePeopleMain = document.querySelector('.selectCalculatePeopleMain');
+const selectCalculatePeople = document.querySelectorAll('.selectCalculatePeople')
+
+const removeCalcItem = () => {
+  calculateDescpItem.forEach(elem => {
+    elem.classList.remove('active')
+  })
+}
+const calculateSliderCheak = () => {
+
+  if (sliderTime.value < 20) {
+    removeCalcItem()
+    calculateDescpItem[0].classList.add('active');
+    dataBigForm.longTimeEvent = '1 час'
+  } else if (sliderTime.value >= 20 && sliderTime.value <= 39) {
+    removeCalcItem()
+    calculateDescpItem[1].classList.add('active');
+    dataBigForm.longTimeEvent = '2 часа'
+  } else if (sliderTime.value >= 40 && sliderTime.value <= 59) {
+    removeCalcItem()
+    calculateDescpItem[2].classList.add('active');
+    dataBigForm.longTimeEvent = '3 часа'
+  } else if (sliderTime.value >= 60 && sliderTime.value <= 79) {
+    removeCalcItem()
+    calculateDescpItem[3].classList.add('active');
+    dataBigForm.longTimeEvent = '4 часа'
+  } else if (sliderTime.value >= 80) {
+    removeCalcItem()
+    calculateDescpItem[4].classList.add('active');
+    dataBigForm.longTimeEvent = '5 часов и больше'
+  }
+}
+sliderTime.addEventListener('mousemove', () => {
+  console.log(dataBigForm);
+  calculateSliderCheak()
+})
+
+
+// доп услуги
+const dopServices = () => {
+  servInput.forEach((elem, index) => {
+    if (elem.checked) {
+      dataBigForm[0].dopEvent.push(elem.name)
+      console.log(dataBigForm[0]);
+    }
+  })
+}
+
+servInput.forEach(elem => {
+  elem.onclick = () => {
+    dataBigForm[0].dopEvent = []
+    dopServices()
+  }
+})
+//данные селектов 
+
+const resetSelectPeople = () => {
+  selectCalculatePeople.forEach(elem => {
+    elem.classList.remove('d-none')
+  })
+}
+
+const selectEvent = () => {
+
+  // selectEventItem.forEach((elem, index) => {
+  if (selectEventMain.textContent === 'Юбилей') {
+    dataBigForm[0].mainEvent = 'Юбилей'
+    resetSelectPeople()
+    selectCalculatePeople[1].classList.add('d-none');
+    selectCalculatePeople[2].classList.add('d-none');
+    selectCalculatePeople[6].classList.add('d-none');
+    selectCalculatePeople[7].classList.add('d-none');
+
+  } else if (selectEventMain.textContent === 'Свадьба') {
+    dataBigForm[0].mainEvent = 'Свадьба'
+    resetSelectPeople()
+    selectCalculatePeople[3].classList.add('d-none');
+
+  } else if (selectEventMain.textContent === 'Корпоратив' ) {
+    resetSelectPeople()
+    selectCalculatePeople[1].classList.add('d-none');
+    selectCalculatePeople[2].classList.add('d-none');
+
+  } else if (selectEventMain.textContent === 'Другое') {
+    dataBigForm[0].mainEvent = 'Другое'
+    resetSelectPeople()
+    selectCalculatePeople[3].classList.add('d-none');
+  }
+  // })
+}
+
+selectEventItem.forEach((elem, index) => {
+  elem.onclick = () => {
+    selectEvent();
+  }
+});
+selectEvent()
--- app/js/home.js
+++ app/js/home.js
@@ -1,32 +1,3 @@
-let select = function () {
-  let selectHeader = document.querySelectorAll('.select__header');
-  let selectItem = document.querySelectorAll('.select__item');
-
-  selectHeader.forEach(item => {
-      item.addEventListener('click', selectToggle)
-  });
-
-  selectItem.forEach(item => {
-      item.addEventListener('click', selectChoose)
-  });
-
-  function selectToggle() {
-      this.parentElement.classList.toggle('is-active');
-  }
-
-  function selectChoose() {
-      let text = this.innerText,
-          select = this.closest('.select'),
-          currentText = select.querySelector('.select__current');
-      currentText.innerText = text;
-      select.classList.remove('is-active');
-
-  }
-
-};
-
-
-select();
 
 
 
@@ -37,22 +8,71 @@ const body = document.querySelector('body');
 const modalClose = document.querySelectorAll('.modal-close');
 const openModalContact = document.querySelectorAll('.openModalContact');
 const modalBg = document.querySelector('.modal-bg');
-const modalContact = document.querySelector('.modal-contact')
+const modalContact = document.querySelector('.modal-contact');
+const modalEvent = document.querySelector('.modal-event');
 
+// modalEvent
+const activitiesCard = document.querySelectorAll('.activities__card');
+const modalEventH3 = document.querySelector('.modal-event h3');
+const modalEventP = document.querySelector('.modal-event p');
+const modalEventPriceFirst = document.querySelector('.modal-event-price-first')
+const modalEventPriceSecond = document.querySelector('.modal-event-price-second')
+
+const activitiesCardDescp = document.querySelectorAll('.activities__card-descp');
+
+const modalEventData = [
+    {
+        title: '1',
+        text: 'Проведение свадьбы вместе с диджеем, 6 часов (все конкурсы, музыка и время проведения согласовываются индивидуально)',
+        onePrice: 'Стоимость: от 18 000Р',
+        twoPrice: 'Стоимость выездной регистрации с музыкальным сопровождением: от 7 000Р',
+    },
+    {
+        title: '2',
+        text: 'Проведение юбилея с диджеем, от 2х часов (все конкурсы, музыка и время проведения согласовываются индивидуально)',
+        onePrice: 'Стоимость: от 12 000Р',
+
+    },
+    {
+        title: '3',
+        text: 'Проведение выпускного с диджеем, от 2х часов (все конкурсы, музыка и время проведения согласовываются индивидуально)',
+        onePrice: 'Стоимость: от 10 000Р ',
+        twoPrice: 'Выпускной 11-х классов. Проведение выпускного с диджеем, от 5 часов (все конкурсы, музыка и время проведения согласовываются индивидуально). Стоимость: от 25 000р',
+    },
+    {
+        title: '4',
+        text: 'Проведение корпоратива с диджеем, дедом морозом и снегурочкой, от 5 часов (все конкурсы, музыка и время проведения согласовываются индивидуально)',
+        onePrice: 'Стоимость: от 25 000Р',
+    },
+    {
+        title: '5',
+        text: 'fhsd fjkhsdfj sdf',
+        onePrice: 'Стоимость: от 18 000Р  5',
+        twoPrice: 'Стоимость выездной регистрации: от 7 000Р 2',
+    },
+    {
+        title: '6',
+        text: 'Индивидуальный сценарий, любая тематика. Отлично подходит для празднования дня рождения или юбилея! Музыкальное сопровождение.',
+        onePrice: 'Стоимость: от 15 000Р',
+    },
+]
+
+
+// modalEvent end
 
 const openModal = () => {
     modalBg.classList.add('active');
     body.classList.add('active')
 }
-
 const closeModal = () => {
     modalBg.classList.remove('active');
     body.classList.remove('active')
     modalContact.classList.remove('active')
+    modalEvent.classList.remove('active')
 }
-
 openModalContact.forEach(elem => {
     elem.onclick = () => {
+        closeModal()
         openModal()
         modalContact.classList.add('active');
     }
@@ -65,3 +85,17 @@ modalClose.forEach(elem => {
         closeModal()
     }
 });
+
+activitiesCard.forEach((elem, index) => {
+    elem.onclick = () => {
+        closeModal()
+        openModal()
+        modalEvent.classList.add('active');
+        modalEventH3.textContent = activitiesCard[index].textContent;
+        modalEventP.textContent = modalEventData[index].text;
+        modalEventPriceFirst.textContent = modalEventData[index].onePrice;
+        if (modalEventData[index].twoPrice) {
+            modalEventPriceSecond.textContent = modalEventData[index].twoPrice;
+        }
+    }
+})
